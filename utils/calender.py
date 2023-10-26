8169f140b09c5330f4a48fe6df89ca5055cfc56b
DanielKuebi <daniflug95@live.at>
2023-10-26 11:36:12 +0200

    updates

--- utils/calender.py
+++ utils/calender.py
@@ -1,24 +1,28 @@
-"""Module to import a predefined calendar for different years. 
-The calendar information should be specified in the data_paths.json file.
+"""Module to import a predefined calendar for different years.
+The path to the calender excel file should be defined in data_paths.py (utils)
 """
 
-import os
-import json
 import pandas as pd
 
-def fetch_calendar(year:int, date_is_index:bool=False) -> pd.DataFrame:
-    """Loads calendar *.xlsx shee from path defined in data_paths.json and returns it as dataframe.
+import data_paths
+
+
+def fetch_calendar(year:int, 
+                   date_is_index:bool=False) -> pd.DataFrame:
+    """Loads calendar *.xlsx shee from path defined in 
+    data_paths.json and returns it as dataframe.
 
     Args:
         year (int): Year of the requested calendar
-        date_is_index (bool, optional): sets date as index if True. Defaults to False.
+        date_is_index (bool, optional): sets date as index if True. 
+        Defaults to False.
 
     Returns:
         pd.DataFrame: Dataframe holing the calendar of a defined year.
     """
     
     # load data path to calendar file
-    calendar_path = "../data/auxiliary/calender_18to23.xlsx"
+    calendar_path = data_paths.CALENDER_PATH
     
     try: 
         df = pd.read_excel(calendar_path, sheet_name=str(year))
@@ -29,26 +33,27 @@ def fetch_calendar(year:int, date_is_index:bool=False) -> pd.DataFrame:
             
         return df
     except Exception as e:
-        print('Could not load calendar. \nDoes the excel worksheet contain the required year and is the path correct?')
+        print('Could not load calendar. Check path and year.')
         print(e)
+
 
 def find_day_type(day:pd.Series) -> int:
     cal_day_type = day['day_type']
     cal_weekday = day['day_of_week']
     
-    # Normweekday
+    # Normweekday -> Tuesday to Thursday (used in traffic planning)
     if cal_day_type==1 and (cal_weekday>=1 and cal_weekday<=3):
         return 0
-    # Weekday
+    # Weekday -> Monday to Friday
     if cal_day_type==1: 
         return 1 
-    # Weekday_vacation / gap day
+    # Weekday during vacation / gap day
     if cal_day_type==4 or cal_day_type==5: 
         return 2
-    #Saturday
+    # Saturday
     if cal_day_type==2 and cal_weekday==5: 
         return 3
-    #Sunday/ Holiday
+    # Sunday/ Holiday -> shops are closed in Germany
     if cal_day_type==3 or cal_weekday==6:
         return 4
     else: 

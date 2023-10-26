c4b213de53b2f40356c25151b32e7fb0a151f39a
DanielKuebi <daniflug95@live.at>
2023-10-26 11:35:39 +0200

    changed to python file for data paths

---
+++ utils/data_paths.py
@@ -0,0 +1,5 @@
+#auxiliary_data
+CALENDER_PATH = "../data/auxiliary/calender_18to23.xlsx"
+MUNICH_BOARDERS = "../data/auxiliary/geodata/munich_boarders.gpkg"
+MUNICH_BOARDERS_EXTENDED = "../data/auxiliary/geodata/munich_boarders_extended.gpkg" # also includes the motorway around Munich
+

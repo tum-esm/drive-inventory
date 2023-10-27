e6d3e3fdd35a3b1e41de82dfe76ce5a39dd622f2
DanielKuebi <daniflug95@live.at>
2023-10-27 10:36:00 +0200

    updated data paths -> absolute path included

--- utils/data_paths.py
+++ utils/data_paths.py
@@ -1,5 +1,13 @@
-#auxiliary_data
-CALENDER_PATH = "../data/auxiliary/calender_18to23.xlsx"
-MUNICH_BOARDERS = "../data/auxiliary/geodata/munich_boarders.gpkg"
-MUNICH_BOARDERS_EXTENDED = "../data/auxiliary/geodata/munich_boarders_extended.gpkg" # also includes the motorway around Munich
+# paths to all relevant files and folders in the project
 
+# get absolute path to the traffic inventory folder
+import pathlib
+abs_path = str(pathlib.Path(__file__).parent.parent.resolve())
+
+#auxiliary_data
+CALENDER_FILE = abs_path + "/data/auxiliary/calender_18to23.xlsx"
+MUNICH_BOARDERS_FILE = abs_path + "/data/auxiliary/geodata/munich_boarders.gpkg"
+MUNICH_BOARDERS_EXTENDED_FILE = abs_path + "/data/auxiliary/geodata/munich_boarders_extended.gpkg" # also includes the motorway around Munich
+
+# visum path´
+VISUM_FOLDER_PATH = abs_path + "/data/restricted_input/visum/"

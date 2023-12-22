8f6cdf5eab56c636a2b62b183fb593bfe8db9ba5
DanielKuebi <daniflug95@live.at>
2023-12-22 13:30:07 +0100

    placeholder

---
+++ utils/hbefa_cold_emissions.py
@@ -0,0 +1,21 @@
+"""_summary_
+"""
+    
+__version__ = 0.1
+__author__ = "Daniel Kühbacher"
+
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+import data_paths
+import excel_calendar
+import traffic_counts
+
+class HbefaColdEmissions: 
+    """_summary_
+    """
+    
+    def __init__(): 
+        pass
+    

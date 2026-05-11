85f5d537281f726dbea7011a15db4011abef700c
MichaelFlei <ge83gup@mytum.de>
2026-05-11 13:22:50 +0200

    added try-except blocks

--- preprocessing/preprocess_bast_locations.py
+++ preprocessing/preprocess_bast_locations.py
@@ -41,82 +41,89 @@ def run():
     # ## Import and clean raw data from *.csv file
 
     # In[ ]:
+    try:
+        # import raw bast location data
+        bast_raw = pd.read_csv(data_path+'bast_locations_bavaria.csv', 
+                            delimiter=';', decimal = ',', encoding='ISO-8859-1')
+    except Exception as e:
+        print("Could not read raw bast location data")
+        return False
 
-    # import raw bast location data
-    bast_raw = pd.read_csv(data_path+'bast_locations_bavaria.csv', 
-                        delimiter=';', decimal = ',', encoding='ISO-8859-1')
+    try:
+        # only use 8+1 counters
+        bast_loc = bast_raw[bast_raw['Erf_Art']== '8+1']
 
-    # only use 8+1 counters
-    bast_loc = bast_raw[bast_raw['Erf_Art']== '8+1']
+        # define relevant columns
+        relevant_columns = ['DZ_Nr', 'Hi_Ri1', 'Hi_Ri2',
+                            'Koor_WGS84_N', 'Koor_WGS84_E']
 
-    # define relevant columns
-    relevant_columns = ['DZ_Nr', 'Hi_Ri1', 'Hi_Ri2',
-                        'Koor_WGS84_N', 'Koor_WGS84_E']
-
-    bast_loc = bast_loc[relevant_columns]
-    bast_loc.head()
-
-
-    # In[ ]:
-
-
-    # divide location data into two directions
-    columns = ['MST_ID', 'DETEKTOR_ID', 'FAHRTRICHTUNG', 
-            'LONGITUDE', 'LATITUDE']
-    row_lst = []
-
-    def coordinate_offset(direction, lon, lat): 
-        offset = 0.0005
-        match direction:
-            case 'N':
-                return {'LONGITUDE': lon, 
-                        'LATITUDE' : lat+offset}
-            case 'O':
-                return {'LONGITUDE': lon-offset, 
-                        'LATITUDE' : lat}
-            case 'S':
-                return {'LONGITUDE': lon, 
-                        'LATITUDE' : lat-offset}
-            case 'W':
-                return {'LONGITUDE': lon+offset,
-                        'LATITUDE' : lat}
-
-    for idx,row in bast_loc.iterrows():
-        row_dict_R1 = { 'MST_ID': row['DZ_Nr'],
-                        'DETEKTOR_ID': int(str(row['DZ_Nr']) + '1'),
-                        'FAHRTRICHTUNG': row['Hi_Ri1']}
-
-        row_dict_R2 = { 'MST_ID': row['DZ_Nr'],
-                        'DETEKTOR_ID': int(str(row['DZ_Nr']) + '2'),
-                        'FAHRTRICHTUNG': row['Hi_Ri2']}
-
-        row_dict_R1.update(coordinate_offset(direction = row['Hi_Ri1'],
-                                            lon = row['Koor_WGS84_N'],
-                                            lat = row['Koor_WGS84_E']))
-
-        row_dict_R2.update(coordinate_offset(direction = row['Hi_Ri2'],
-                                            lon = row['Koor_WGS84_N'],
-                                            lat = row['Koor_WGS84_E']))
-
-        row_lst.append(row_dict_R1)
-        row_lst.append(row_dict_R2)
-
-    bast = pd.DataFrame(row_lst, columns = columns)
-    bast.head()
+        bast_loc = bast_loc[relevant_columns]
+        bast_loc.head()
 
 
-    # # Save preprocessed bast locations as *.gpkg
-
-    # In[5]:
+        # In[ ]:
 
 
-    bast = gpd.GeoDataFrame(
-        bast, geometry=gpd.points_from_xy(bast.LATITUDE, bast.LONGITUDE))
-    bast.drop(['LATITUDE', 'LONGITUDE'], axis = 1, inplace = True)
-    bast = bast.set_crs(epsg='4326')
+        # divide location data into two directions
+        columns = ['MST_ID', 'DETEKTOR_ID', 'FAHRTRICHTUNG', 
+                'LONGITUDE', 'LATITUDE']
+        row_lst = []
 
-    # save data
-    bast.to_file(data_path + "bast_locations_cleaned.gpkg", driver="GPKG")
+        def coordinate_offset(direction, lon, lat): 
+            offset = 0.0005
+            match direction:
+                case 'N':
+                    return {'LONGITUDE': lon, 
+                            'LATITUDE' : lat+offset}
+                case 'O':
+                    return {'LONGITUDE': lon-offset, 
+                            'LATITUDE' : lat}
+                case 'S':
+                    return {'LONGITUDE': lon, 
+                            'LATITUDE' : lat-offset}
+                case 'W':
+                    return {'LONGITUDE': lon+offset,
+                            'LATITUDE' : lat}
+
+        for idx,row in bast_loc.iterrows():
+            row_dict_R1 = { 'MST_ID': row['DZ_Nr'],
+                            'DETEKTOR_ID': int(str(row['DZ_Nr']) + '1'),
+                            'FAHRTRICHTUNG': row['Hi_Ri1']}
+
+            row_dict_R2 = { 'MST_ID': row['DZ_Nr'],
+                            'DETEKTOR_ID': int(str(row['DZ_Nr']) + '2'),
+                            'FAHRTRICHTUNG': row['Hi_Ri2']}
+
+            row_dict_R1.update(coordinate_offset(direction = row['Hi_Ri1'],
+                                                lon = row['Koor_WGS84_N'],
+                                                lat = row['Koor_WGS84_E']))
+
+            row_dict_R2.update(coordinate_offset(direction = row['Hi_Ri2'],
+                                                lon = row['Koor_WGS84_N'],
+                                                lat = row['Koor_WGS84_E']))
+
+            row_lst.append(row_dict_R1)
+            row_lst.append(row_dict_R2)
+
+        bast = pd.DataFrame(row_lst, columns = columns)
+        bast.head()
+
+
+        # # Save preprocessed bast locations as *.gpkg
+
+        # In[5]:
+
+
+        bast = gpd.GeoDataFrame(
+            bast, geometry=gpd.points_from_xy(bast.LATITUDE, bast.LONGITUDE))
+        bast.drop(['LATITUDE', 'LONGITUDE'], axis = 1, inplace = True)
+        bast = bast.set_crs(epsg='4326')
+
+        # save data
+        bast.to_file(data_path + "bast_locations_cleaned.gpkg", driver="GPKG")
+    except Exception as e:
+        print("Could not process bast locations")
+        return False
 
     return True
 if __name__ == "__main__":
